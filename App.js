ddbae5a38bfd02a24cf597b5b68438ac5b3ecf2b
hungnt <hungnt@smartosc.com>
2023-05-19 11:50:51 +0700

    update splash

--- App.js
+++ App.js
@@ -44,6 +44,10 @@ export default function App() {
 
   useEffect(() => {
     registerForPushNotificationsAsync().then((token) => {
+      if (!token) {
+        return;
+      }
+
       saveCustomer(token);
     });
     return () => {};
