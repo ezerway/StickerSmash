5d2498642301b525d1d229c4f705ce8b04953676
EzerWay <live.ezerway@gmail.com>
2023-08-23 12:04:12 +0700

    Update share logic

--- app/share.js
+++ app/share.js
@@ -1,15 +1,4 @@
-import { useLocalSearchParams } from 'expo-router';
-import { View, Text, SafeAreaView, Image } from 'react-native';
-
+import AddFeedModal from './add-feed-modal';
 export default function Share() {
-  const { url } = useLocalSearchParams();
-
-  return (
-    <SafeAreaView>
-      <View>
-        <Text>{url}</Text>
-        <Image style={{ width: 100, height: 100 }} source={{ uri: url }} />
-      </View>
-    </SafeAreaView>
-  );
+  return <AddFeedModal />;
 }
